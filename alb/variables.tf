8ff607f27c5cea399f9d82a315c56f9d9d9865f2
Chidi Ukaegbu <chidex.ukaegbu4real@gmail.com>
2023-11-27 22:46:34 -0600

    created alb module

---
+++ alb/variables.tf
@@ -0,0 +1,9 @@
+# alb variables 
+variable "project_name" {}
+variable "environment" {}
+variable "alb_security_group_id" {}
+variable "public_subnet_az1.id" {}
+variable "public_subnet_az2.id" {}
+variable "target_type" {}
+variable "vpc_id" {}
+variable "certificate_arn" {}
