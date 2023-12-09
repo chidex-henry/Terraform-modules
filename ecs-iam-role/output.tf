f0734439906f4a385073197e24548e78d9062a5f
Chidi Ukaegbu <chidex.ukaegbu4real@gmail.com>
2023-12-09 11:07:22 -0600

    created iam role

--- ecs-iam-role/output.tf
+++ ecs-iam-role/output.tf
@@ -1,4 +1,4 @@
 # export the ecs task execution role arn 
 output "ecs_task_execution_role_arn" {
-  value = aws_iam_policy.ecs_task_execution_policy.arn 
+  value = aws_iam_role.ecs_task_execution_role.arn 
 }
