b0746cf9cb97f5aaf74bcf86622da043e483e70a
Chidi Ukaegbu <chidex.ukaegbu4real@gmail.com>
2023-12-23 14:15:54 -0600

    module added

--- ecs-iam-role/main.tf
+++ ecs-iam-role/main.tf
@@ -31,7 +31,7 @@ data "aws_iam_policy_document" "ecs_task_execution_policy_document" {
     ]
 
     resources = [
-      "arn:aws:s3:::${var.project_name}-${var.env_file_bucket_name}/*"
+      "arn:aws:s3:::rentzone-rentzone-henry-ecs-env-file-bucket/rentzone.env"
     ]
   }
 
@@ -41,7 +41,7 @@ data "aws_iam_policy_document" "ecs_task_execution_policy_document" {
     ]
 
     resources = [
-      "arn:aws:s3:::${var.project_name}-${var.env_file_bucket_name}"
+      "arn:aws:s3:::rentzone-rentzone-henry-ecs-env-file-bucket"
     ]
   }
 }
