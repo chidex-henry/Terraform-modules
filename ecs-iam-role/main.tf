1e91927ab66ded58638f87665ef97f110babfe7e
Chidi Ukaegbu <chidex.ukaegbu4real@gmail.com>
2023-12-25 07:16:32 -0600

    created ECS IAM Module

--- ecs-iam-role/main.tf
+++ ecs-iam-role/main.tf
@@ -31,7 +31,7 @@ data "aws_iam_policy_document" "ecs_task_execution_policy_document" {
     ]
 
     resources = [
-      "arn:aws:s3:::rentzone-rentzone-henry-ecs-env-file-bucket/rentzone.env"
+      "arn:aws:s3:::${var.project_name}-${var.env_file_bucket_name}/*"
     ]
   }
 
@@ -41,7 +41,7 @@ data "aws_iam_policy_document" "ecs_task_execution_policy_document" {
     ]
 
     resources = [
-      "arn:aws:s3:::rentzone-rentzone-henry-ecs-env-file-bucket"
+      "arn:aws:s3:::${var.project_name}-${var.env_file_bucket_name}"
     ]
   }
 }
@@ -49,17 +49,17 @@ data "aws_iam_policy_document" "ecs_task_execution_policy_document" {
 # create iam policy
 resource "aws_iam_policy" "ecs_task_execution_policy" {
   name   = "${var.project_name}-${var.environment}-ecs-task-execution-role-policy"
-  policy = data.aws_iam_policy_document.ecs_task_execution_policy_document.json
+  policy = data.aws_iam_policy_document.assume_role_policy.json 
 }
 
 # create an iam role
 resource "aws_iam_role" "ecs_task_execution_role" {
   name               = "${var.project_name}-${var.environment}-ecs-task-execution-role"
-  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json 
 }
 
 # attach ecs task execution policy to the iam role
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
   role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = aws_iam_policy.ecs_task_execution_policy.arn
+  policy_arn = aws_iam_policy.ecs_task_execution_policy.arn 
 }
