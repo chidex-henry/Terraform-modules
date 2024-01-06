9a23330d965a29e9fc1ecd895e1e8aaa0c70a0b0
Chidi Ukaegbu <chidex.ukaegbu4real@gmail.com>
2024-01-06 12:13:45 -0600

    added alb

--- alb/main.tf
+++ alb/main.tf
@@ -14,7 +14,7 @@ resource "aws_lb" "application_load_balancer" {
 
 # create target group
 resource "aws_lb_target_group" "alb_target_group" {
-  name        = "${var.project_name}-${var.environment}-dev-tg"
+  name        = "${var.project_name}-${var.environment}-tg"
   target_type = var.target_type 
   port        = 80
   protocol    = "HTTP"
